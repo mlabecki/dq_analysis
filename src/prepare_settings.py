1bfb754a96a2c182d919c4bccc3277d8abc8e48d
mlabecki <3i14159265@gmail.com>
2024-06-15 16:20:07 -0400

    Corrected paths to be relative to root folder

--- src/prepare_settings.py
+++ src/prepare_settings.py
@@ -5,11 +5,10 @@ class ReadSettings:
 
     def __init__(self):
         
-        self.src_dir = os.getcwd()
-        self.root_dir = os.path.join(self.src_dir, '..')
-        self.freddie_mac_dir = os.path.join(self.root_dir, 'data/freddie_mac/standard')
+        # All paths must be relative to Github repo root folder for Streamlit cloud deployment
 
-        self.layout_file = os.path.join(self.root_dir, 'cfg', main_settings['layout_file'])
+        self.freddie_mac_dir = os.path.join('data/freddie_mac/standard')
+        self.layout_file = os.path.join('cfg', main_settings['layout_file'])
         self.quarterly_file_prefix = main_settings['quarterly_file_prefix']
         self.monthly_file_prefix = main_settings['monthly_file_prefix']
         self.dq_subdir = main_settings['dq_subdir']
